2db200b85bc0dcc833aa4d6023234671dca742ef
Ben-Limo <bensokiplimo@gmail.com>
2024-05-29 11:40:21 +0300

    add a check

--- src/coin-counter1.js
+++ src/coin-counter1.js
@@ -23,6 +23,9 @@ const coinCounter1 = (amount) => {
   // Find the largest coin that is less than or equal to the amount
   for (const coin in coinValues) {
     const coinValue = coinValues[coin];
+
+    if (amount >= coinValue) {
+    }
   }
 
   return result;
