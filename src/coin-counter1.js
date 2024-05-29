7413a7be090233586f2db6e189daf0b44e30ba0e
Ben-Limo <bensokiplimo@gmail.com>
2024-05-29 11:34:50 +0300

    define the coin values

--- src/coin-counter1.js
+++ src/coin-counter1.js
@@ -1,1 +1,9 @@
-const coinCounter1 = (amount) => {};
+const coinCounter1 = (amount) => {
+  // Define coin values
+  const coinValues = {
+    quarters: 25,
+    dimes: 10,
+    nickels: 5,
+    pennies: 1,
+  };
+};
