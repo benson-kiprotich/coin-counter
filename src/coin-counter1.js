688626bc616a0aa2fa8ca80ed543fe42b0cb8ef7
Ben-Limo <bensokiplimo@gmail.com>
2024-05-29 11:35:33 +0300

    define the base case

--- src/coin-counter1.js
+++ src/coin-counter1.js
@@ -6,4 +6,14 @@ const coinCounter1 = (amount) => {
     nickels: 5,
     pennies: 1,
   };
+
+  // Base case: when amount becomes zero, return an object with all coins set to zero
+  if (amount === 0) {
+    return {
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0,
+    };
+  }
 };
